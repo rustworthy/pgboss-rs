9c38ebedbfa285535439e079e599fca923573261
Pavel Mikhalkevich <paul.michelle.prod@gmail.com>
2024-08-18 23:04:36 +0400

    Create job table

--- src/stmt.rs
+++ src/stmt.rs
@@ -71,6 +71,36 @@ fn create_subscription_table(schema: &str) -> String {
     )
 }
 
+fn create_job_table(schema: &str) -> String {
+    format!(
+        "
+        CREATE TABLE IF NOT EXISTS {schema}.job (
+            id uuid not null default gen_random_uuid(),
+            name text not null,
+            priority integer not null default(0),
+            data jsonb,
+            state {schema}.job_state not null default('{}'),
+            retry_limit integer not null default(0),
+            retry_count integer not null default(0),
+            retry_delay integer not null default(0),
+            retry_backoff boolean not null default false,
+            start_after timestamp with time zone not null default now(),
+            started_on timestamp with time zone,
+            singleton_key text,
+            singleton_on timestamp without time zone,
+            expire_in interval not null default interval '15 minutes',
+            created_on timestamp with time zone not null default now(),
+            completed_on timestamp with time zone,
+            keep_until timestamp with time zone NOT NULL default now() + interval '14 days',
+            output jsonb,
+            dead_letter text,
+            policy text      
+        ) PARTITION BY LIST (name);
+        ",
+        JobState::Created
+    )
+}
+
 fn insert_version(schema: &str) -> String {
     format!(
         "INSERT INTO {schema}.version (version) VALUES ({}) ON CONFLICT DO NOTHING;",
@@ -118,6 +148,7 @@ pub(crate) fn compile_ddl(schema: &str) -> String {
             create_version_table(schema),
             create_queue_table(schema),
             create_subscription_table(schema),
+            create_job_table(schema),
             // ...
             insert_version(schema),
         ],
