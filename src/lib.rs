8d88a137a0741153b1e8eb3c8b730fc67de158c2
Pavel Mikhalkevich <paul.michelle.prod@gmail.com>
2024-10-03 21:51:10 +0400

    Update qname in the docs

--- src/lib.rs
+++ src/lib.rs
@@ -19,11 +19,11 @@
 //!
 //! // Build a job and ...
 //! let job = Job::builder()
-//!     .queue_name("jobtype")                         // which queue this job should be sent to
+//!     .queue_name("qname")                           // which queue this job should be sent to
 //!     .data(json!({"key": "value"}))                 // arbitrary json, your job's payload
 //!     .priority(10)                                  // will be consumer prior to those with lower priorities
 //!     .retry_limit(1)                                // only retry this job once
-//!     .dead_letter("jobtype_dlq")                    // send to this queue when retry limit exceeded
+//!     .dead_letter("qname_dlq")                      // send to this queue when retry limit exceeded
 //!     .retry_delay(Duration::from_secs(60 * 5))      // do not retry immediately after failure
 //!     .expire_in(Duration::from_secs(60 * 5))        // only give the worker 5 minutes to complete the job
 //!     .retain_for(Duration::from_secs(60 * 60 * 24)) // do not archive for at least 1 day
@@ -37,7 +37,7 @@
 //!
 //! // Consume from the queue.
 //! let fetched_job = c
-//!     .fetch_job("jobtype")
+//!     .fetch_job("qname")
 //!     .await
 //!     .expect("no error")
 //!     .expect("a job");
